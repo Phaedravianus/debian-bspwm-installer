a2b285d6ad33c61a1e331fd97e7b0852c5d8238f
Phaedravianus <112165910+Phaedravianus@users.noreply.github.com>
2023-07-21 12:02:16 +0300

    Update ins-config.sh
    improving routing

--- ins-config.sh
+++ ins-config.sh
@@ -6,23 +6,21 @@ sudo mv ~/.config/sxhkd/sxhkdrc ~/.config/sxhkd/sxkdrc.bak
 sudo mv ~/debian-installer/config/sxhkdrc ~/.config/sxhkd/
 
 #rofi
-sudo mkdir ~/.config/rofi 
-sudo mv ~/debian-installer/config/rofi/** ~/.config/rofi
+sudo mv ~/debian-installer/config/rofi ~/.config/rofi
 
 #kitty
-sudo mv ~/debian-installer/config/kitty/** ~./config/kitty
+sudo mv ~/debian-installer/config/kitty/kitty.conf ~./config/kitty
 
 #dunst
 sudo mkdir ~/.config/dunst
-sudo mv ~/debian-installer/config/dunst/** ~/.config/dunst
+sudo mv ~/debian-installer/config/dunst/dunstrc ~/.config/dunst
 
 #polybar
 sudo mkdir ~/.config/polybar
-sudo mv ~/debian-installer/config/polybar/** ~/.config/polybar
+sudo mv ~/debian-installer/config/polybar/config.ini ~/.config/polybar
 
 #background
-sudo mkdir ~/.config/Backgrounds
-sudo mv ~/debian-installer/config/Backgrounds/** ~/.config/Backgrounds 
+sudo mv ~/debian-installer/config/Backgrounds ~/.config/Backgrounds 
 
 #bdprochot
 sudo mv ~/debian-installer/bdprochot.sh ~/
@@ -50,9 +48,9 @@ sudo mv ~/debian-installer/lightdm.conf /etc/lightdm/
 sudo mv ~/debian-installer/slick-greeter.conf /etc/lightdm/
 
 #Newsboat
-sudo mkdir ~/.newsboat
-sudo mv ~/debian-installer/.newsboat/config ~/.newsboat/
-sudo mv ~/debian installer/.newsboat/url ~/.newsboat/
+#sudo mkdir ~/.newsboat
+#sudo mv ~/debian-installer/.newsboat/config ~/.newsboat/
+#sudo mv ~/debian installer/.newsboat/url ~/.newsboat/
 
 ################################################################
 echo "Configs are installed, You should Reboot Now!"
